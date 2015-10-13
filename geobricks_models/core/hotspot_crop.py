1d862d1dcb93cd5d6f47a0d1ac5c54f20332607a
simonefenix <simone.murzilli@fao.org>
2015-10-13 17:26:42 +0200

    update

--- geobricks_models/core/hotspot_crop.py
+++ geobricks_models/core/hotspot_crop.py
@@ -2,24 +2,116 @@ from geobricks_common.core.filesystem import get_raster_path
 from geobricks_common.core.filesystem import get_vector_path
 from geobricks_common.core.log import logger
 
+from geobricks_models.utils.filter_shapefile import cut_shp
+from geobricks_models.utils import crop_raster
+from geobricks_models.utils.mask_threshold import mask_raster_by_thresholds
+
 log = logger(__file__)
 
-def test():
 
-    raster_path = get_raster_path({
-        "datasource": ["geoserver"],
-        'workspace': 'ndvi_anomaly',
-        'layerName': 'ndvi_anomaly_1km_mod13a3_200803_3857'
-    })
+def calc_hotspot(obj):
+    print obj
 
-    vector_path = get_vector_path({
-        "datasource": ["storage"],
-        'layerName': 'gaul1_nena_4326'
-    })
+    # vector object
+    vector = obj['vector']
+    raster = obj['raster']
+    model_options = obj['model_options']
 
-    print raster_path
-    print vector_path
+    # create new AOI shapefile
+    shp_path = get_vector_path(vector)
+    aoi_shp_path = cut_shp(shp_path, vector['filter'])
+
+    # extract bbox AOI indicator (i.e. NDVI)
+    indicator_path = get_raster_path(raster[0])
+    indicator_aoi_path = crop_raster.crop_by_shapefile_bbox(indicator_path, aoi_shp_path)
+
+    # extract bbox AOI cultivation/crop (i.e. WHEAT)
+    cultivation_path = get_raster_path(raster[1])
+    cultivation_aoi_path = crop_raster.crop_by_shapefile_bbox(cultivation_path, aoi_shp_path)
+
+    # indicator_mask AOI indicator with threshold
+    min = model_options['threshold']['min'] if 'min' in model_options['threshold'] else None
+    max = model_options['threshold']['max'] if 'max' in model_options['threshold'] else None
+    indicator_aoi_mask_path = mask_raster_by_thresholds(indicator_aoi_path, min, max)
+
+    # reproject indicator to cultivation crs
+
+    # resample ndvi to wheat
+
+    # hotspot_aoi_layer = multiply ndvi_mask * wheat
+
+    # for each gaul1 cut the hotspot_aoi_layer
+
+    # calc for each gaul1 the zonalsum
+
+    # raster_path = get_raster_path({
+    #     'datasource': ['geoserver'],
+    #     'workspace': 'ndvi_anomaly',
+    #     'layerName': 'ndvi_anomaly_1km_mod13a3_200803_3857'
+    # })
+    #
+    # vector_path = get_vector_path({
+    #     'datasource': ['storage'],
+    #     'layerName': 'gaul1_nena_4326'
+    # })
+
+    # print raster_path
+    # print vector_path
 
 
 
-test()
+
+obj = {
+    'raster': [
+        {
+            'datasource': ['geoserver'],
+            'workspace': 'ndvi_anomaly',
+            'layerName': 'ndvi_anomaly_1km_mod13a3_200803_3857'
+        },
+        {
+            'datasource': ['storage'],
+            'layerName': 'wheat_area_4326'
+        }
+    ],
+    # TODO check the other vector definition
+    'vector': {
+        'datasource': 'storage',
+        'type': 'shapefile',
+        'layerName': 'gaul1_nena_4326',
+
+        # TODO: geostatistics example
+        # 'options': {
+        #     'layer': 'gaul1_2015_4326',  # required (table or table alias)
+        #     'column': 'adm0_name',  # required (column or column_alias)
+        #     'codes': ['Italy'],
+        #     'groupby': ['adm1_code', 'adm1_name']  # optional used to get subcodes (i.e. get all italian's region)
+        # },
+
+        #  TODO: check the other vector filter definition (this should be the same as in geostatistics?)
+        'filter': {
+            'column': 'adm1_code',
+            'codes': [61525, 2755],
+            # TODO: find a proper name instead of output (this should be the same as in geostatistics?)
+            'output': ['adm1_name', 'adm1_code']
+        },
+        # TODO check other vector label
+    },
+    # TODO check the other stats
+    'stats': {
+        'zonalsum': {
+            # weight of the zonalsum pixel
+            'weight': 10
+        }
+    },
+
+    # TODO: is it the right name?
+    'model_options': {
+        'threshold': {
+            'min': None,
+            'max': -30
+        }
+    },
+
+}
+
+calc_hotspot(obj)
--- geobricks_models/utils/mask_threshold.py
+++ geobricks_models/utils/mask_threshold.py
@@ -1,15 +1,19 @@
 import rasterio
 import numpy as np
 from geobricks_common.core.log import logger
-from eco_countries_demo.processing.utils_rasterio import initialize_rasterio_raster
+from geobricks_common.core.filesystem import create_tmp_filename
+
+from geobricks_models.utils.utils_rasterio import initialize_rasterio_raster
 
 log = logger(__file__)
 
 
 # cut the raster layer by a max_threshold i.e. < -30%
-def cut_by_threshold(input_raster, output_path, max_threshold):
+def mask_raster_by_thresholds(input_raster, min=None, max=None, nodata=None):
 
-    output_layer_path = output_path + "/test.tif"
+    print min, max, nodata
+
+    output_layer_path = create_tmp_filename('geotiff', 'indicator_mask_')
 
     r = rasterio.open(input_raster)
     r_data = r.read_band(1).astype(float).flatten()
@@ -18,21 +22,34 @@ def cut_by_threshold(input_raster, output_path, max_threshold):
     data, kwargs = initialize_rasterio_raster(r, rasterio.float32)
 
     # get nodata value and min and min max values
-    nodata = r.meta['nodata'] if 'nodata' in r.meta else None
+    nodata_computed = r.meta['nodata'] if 'nodata' in r.meta else None
     min_computed = np.nanmin(r_data)
     max_computed = np.nanmax(r_data)
 
+    # ge the right thresholds
+    if min is None:
+        min = min_computed
+
+    if max is None:
+        max = max_computed
+
+    if nodata is None:
+        nodata = nodata_computed
+
     # compute treshold
-    data = compute_thresholds(r_data, min_computed, max_threshold, nodata)
+    data = _compute_thresholds(r_data, min, max, nodata)
+
+    print min, max, nodata
+    print data
 
     # Writing output file
-    output_layer_path = output_path + "/test.tif"
-    print "Writing: ", output_layer_path
+    log.info("Mask_raster_by_thresholds: " + output_layer_path)
     with rasterio.open(output_layer_path, 'w', **kwargs) as dst:
         dst.write_band(1, data.reshape(r.shape[0], r.shape[1]).astype(rasterio.float32))
 
+    return output_layer_path
 
-def compute_thresholds(array, min, max, nodata=None):
+def _compute_thresholds(array, min, max, nodata=None):
 
     index = (array > min) & (array <= max) & (array != nodata)
 
