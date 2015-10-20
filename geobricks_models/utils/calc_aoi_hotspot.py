0ffe57c45f5ab85aa49598c8ffe73ca8f8a96961
simonefenix <simone.murzilli@fao.org>
2015-10-20 13:44:15 +0200

    .

--- geobricks_models/utils/calc_aoi_hotspot.py
+++ geobricks_models/utils/calc_aoi_hotspot.py
@@ -19,13 +19,13 @@ def multiply_raster(input_raster1, input_raster2, output_path=None):
     r2 = rasterio.open(input_raster2)
     r_data2 = r2.read_band(1).astype(float).flatten()
 
-
     nodata1 = r1.meta['nodata'] if 'nodata' in r1.meta else None
     nodata2 = r2.meta['nodata'] if 'nodata' in r2.meta else None
 
     index1 = (r_data1 != nodata1)
     index2 = (r_data2 != nodata2)
 
+    r_data1 = index1 * r_data1
     r_data2 = index2 * r_data2
 
     # initialize output raster
